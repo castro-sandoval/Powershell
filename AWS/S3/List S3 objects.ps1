1466159f4cc6559b7a160f3e295e37f6b69c74e0
Sandoval <159578817+castro-sandoval@users.noreply.github.com>
2024-02-09 19:40:12 -0500

    Update List S3 objects.ps1

--- AWS/S3/List S3 objects.ps1
+++ AWS/S3/List S3 objects.ps1
@@ -5,7 +5,7 @@ $SecretKey='*************'
 Set-AWSCredential -AccessKey $AccessKey -SecretKey $SecretKey
 
 
-$AllS3Backups = (Get-S3Object -BucketName lcp2-sql-backups-us-east-1 -KeyPrefix "/RDS_DEV/" | Where-Object {$_.Key -like "*StorageEntities.bak" } ) | Select key, @{name="date";expression = {[System.IO.Path]::GetFileNameWithoutExtension($_.key).split("_")[1]} } | Sort-Object date -Descending
+$AllS3Backups = (Get-S3Object -BucketName bucketname -KeyPrefix "/RDS_DEV/" | Where-Object {$_.Key -like "*StorageEntities.bak" } ) | Select key, @{name="date";expression = {[System.IO.Path]::GetFileNameWithoutExtension($_.key).split("_")[1]} } | Sort-Object date -Descending
 foreach ($AllS3Backup in $AllS3Backups)
 {
     "arn:aws:s3:::"+$AllS3Backup.key
